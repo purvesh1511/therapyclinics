d3913e382ba5913653e39b52a061348e6c40676c
Sagar Desai <sagar@ezcontacts.com>
2026-02-08 14:37:10 +0530

    Meta tag add in home and partner page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
-import Header from "@/components/layout/Header";
 import HeroSection from "@/components/home/HeroSection"
 import TrendingHealth from "@/components/home/TrendingHealth";
 import TrendingSpeciality from "@/components/home/TrendingSpeciality";
 import PopularSpecialties from "@/components/home/PopularSpecialities";
 import HealthCarePractice from "@/components/home/HealthCarePractice";
-import Footer from "@/components/layout/Footer";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Therapy Clinics + No. 1 Health &amp; Wellness Platform in the UK",
+  description: "Find trusted clinics for therapy, health, wellness &amp; recovery. Start by searching for a specific practitioner, service or health concern in your local area.",
+};
 
 export default function Home() {
   return (
--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -5,6 +5,12 @@ import ProblemSection from "@/components/partners/ProblemSection";
 import ProviderSection from "@/components/partners/ProviderSection";
 import SolutionSection from "@/components/partners/SolutionSection";
 import StatsSection from "@/components/partners/StatsSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Partner - Therapy Clinics + No. 1 Health &amp; Wellness Platform in the UK",
+  description: "Find trusted clinics for therapy, health, wellness &amp; recovery. Start by searching for a specific practitioner, service or health concern in your local area.",
+};
 
 export default function Partners() {
   return (
